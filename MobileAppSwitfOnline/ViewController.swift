2a3f10d5210f9802f191a08929a2a748700dba02
memo figueredo <memof80@gmail.com>
2026-03-13 17:47:33 -0500

    Initial Commit

---
+++ MobileAppSwitfOnline/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileAppSwitfOnline
+//
+//  Created by Memo Figueredo on 13/3/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
